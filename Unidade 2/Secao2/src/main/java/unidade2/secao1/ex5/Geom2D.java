1acb538f1ce750f1278064af3e8c353b84547720
YagoLisboa <99895960+YagoLisboa@users.noreply.github.com>
2023-07-12 13:14:40 -0300

    Create Geom2D and Triangulo.java
    Using Overridig methods in Java language.

---
+++ Unidade 2/Secao2/src/main/java/unidade2/secao1/ex5/Geom2D.java
@@ -0,0 +1,16 @@
+package unidade2.secao1.ex5;
+
+/**
+ *
+ * @author Yago Lisboa
+ */
+public class Geom2D {
+    double perimetro;
+    double area;
+    double calcPerimetro(){
+        return 0;
+    }
+    double calcArea(){
+        return 0;
+    }
+}
